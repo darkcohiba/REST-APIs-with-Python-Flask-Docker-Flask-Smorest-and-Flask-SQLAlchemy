1c3780c7cac6da9cd490d1ac67b110a5803cd11f
Sam <samuel.waters@gmail.com>
2023-06-07 14:17:39 -0600

    chore: lambda

--- playground.py
+++ playground.py
@@ -1,4 +1,10 @@
 # destructing variables
 person = ("Sam", 42, "AI")
 name, _, job = person
-print(_)
+# using underscore as a variable name to show we want to ignore it
+# print(_)
+
+# what the fuck? Create a head to the array and a tail to the array
+head, *tail = ['a', 'b', 'c', 'd', 'e']
+
+# print(head, tail)
