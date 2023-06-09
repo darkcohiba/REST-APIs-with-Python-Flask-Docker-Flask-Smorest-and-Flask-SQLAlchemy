675f45eebdd62de220deb4f57b7be5679b7147cb
Sam <samuel.waters@gmail.com>
2023-06-09 11:14:27 -0600

    chore: custom error messages

--- custom_errors/code.py
+++ custom_errors/code.py
@@ -3,3 +3,6 @@ class Book:
         self.name = name
         self.page_count = page_count
         self.pages_read = 0
+
+    def __repr__(self) -> str:
+        return f"Book: {self.name}, read {self.pages_read} out of {self.page_count}."
