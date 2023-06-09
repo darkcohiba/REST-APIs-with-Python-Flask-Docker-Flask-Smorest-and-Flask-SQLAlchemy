5eaa845c4a8526cc7928cd2339d12c84b25e776d
Sam <samuel.waters@gmail.com>
2023-06-09 11:26:56 -0600

    chore: custom error messages

--- custom_errors/code.py
+++ custom_errors/code.py
@@ -1,3 +1,7 @@
+# define out TooManyPagesReadError, it will inherit from ValueError. we dont need to anything to it, the message will come from the error call.
+class TooManyPagesReadError(ValueError):
+    pass
+
 class Book:
     def __init__(self, name: str, page_count: int):
         self.name = name
@@ -9,10 +13,17 @@ class Book:
     
 
     def read(self, pages: int):
+        # add a custom error hear to check if pages + pages_read is greater than page count
+        if self.pages_read + pages > self.page_count:
+            raise TooManyPagesReadError(f"You tried to read {self.pages_read + pages} pages, but this book only has {self.page_count} pages!")
         self.pages_read += pages
         print(f"Read {self.pages_read} out of {self.page_count}.")
 
 potter = Book("Harry", 50)
-potter.read(50)
+# potter.read(50)
 # to start if we run this second read 50 we have an output of "Read 100 out of 50.", which isn't possible so goal is to add our custom error to catch this.
-potter.read(50)
+# potter.read(50)
+
+# updated with a try except to catch our new error
+
+
