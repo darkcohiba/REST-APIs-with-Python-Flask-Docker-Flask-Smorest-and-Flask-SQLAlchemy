7b6324d550f699662839f5e8da2015232eb222eb
Sam <samuel.waters@gmail.com>
2023-06-09 11:16:25 -0600

    chore: custom error messages

--- custom_errors/code.py
+++ custom_errors/code.py
@@ -6,3 +6,13 @@ class Book:
 
     def __repr__(self) -> str:
         return f"Book: {self.name}, read {self.pages_read} out of {self.page_count}."
+    
+
+    def read(self, pages: int):
+        self.pages_read += pages
+        print(f"Read {self.pages_read} out of {self.page_count}.")
+
+potter = Book("Harry", 50)
+potter.read(50)
+# to start if we run this second read 50 we have an output of "Read 100 out of 50.", which isn't possible so goal is to add our custom error to catch this.
+potter.read(50)
