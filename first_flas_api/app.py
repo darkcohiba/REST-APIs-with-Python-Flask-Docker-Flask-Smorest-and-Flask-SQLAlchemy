e976e5c7cffc90c74ea61cf4f002f86869b6db1b
Sam <samuel.waters@gmail.com>
2023-06-10 07:51:22 -0600

    chore: started first api

--- first_flas_api/app.py
+++ first_flas_api/app.py
@@ -25,3 +25,17 @@ def post_store():
     request_data = request.get_json()
     stores.append(request_data)
     return {"stores": stores}, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    # print(request_data)
+    for store in stores:
+        print(store["name"])
+        if store["name"] == name.title():
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"]
+            }
+            store["items"].append(new_item)
+    return {"stores": stores}
