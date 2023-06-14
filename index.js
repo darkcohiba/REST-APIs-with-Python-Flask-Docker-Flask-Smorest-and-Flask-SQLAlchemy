ce525aac375e2b67003751d2d2f5b9225e9b8086
Sam <samuel.waters@gmail.com>
2023-06-14 08:35:44 -0600

    chore: added index for student help

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function saturdayFun(activity="roller-skate"){
-    console.log( `This saturday, i want to ${activity}`)
+    return `This saturday, i want to ${activity}`
 }
 
-// console.log(saturdayFun("roller-skate with brandi"));
-saturdayFun()
+console.log(saturdayFun("roller-skate with brandi"));
