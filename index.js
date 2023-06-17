9908acc1b2afb6a9fdf966a91035c6ba79ba6939
Sam <samuel.waters@gmail.com>
2023-06-16 20:58:20 -0600

    chore: adding for the dya

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ function saturdayFun(activity="roller-skate"){
     return `This saturday, i want to ${activity}`
 }
 
-console.log(saturdayFun("roller-skate with brandi"));
+// console.log(saturdayFun("roller-skate with brandi"));
+
+(() => console.log(`This saturday, i want to`))()
+const catFunc = cat => console.log(`This saturday, i want to ${cat}`)
+// catFunc("cat")
+
