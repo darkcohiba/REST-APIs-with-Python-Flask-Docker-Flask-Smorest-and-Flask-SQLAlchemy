9ff036dae259437a6f3ac226c118999a87fc7371
Sam <samuel.waters@gmail.com>
2023-06-19 11:08:23 -0600

    lesson: blueprints/method views

--- first_flas_api/resources/store.py
+++ first_flas_api/resources/store.py
@@ -3,3 +3,9 @@ from flask import request
 from flask.views import MethodView
 from flask_florest import Blueprint, abort
 from db import stores
+
+
+# create blueprint
+blp = Blueprint("stores", __name__, description="Operation on stores")
+
+# create a class out of method views
