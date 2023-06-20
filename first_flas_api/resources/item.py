f56ab64d964fe03cb6442b051b02c7737ca70a8d
Sam <samuel.waters@gmail.com>
2023-06-20 15:23:33 -0600

    lesson: blueprints/method views

---
+++ first_flas_api/resources/item.py
@@ -0,0 +1,29 @@
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_florest import Blueprint, abort
+from db import stores
+
+
+# create blueprint
+blp = Blueprint("stores", __name__, description="Operation on stores")
+
+# create a class out of method views
+# establish the route with the blueprint
+@blp.route("/store/<string:item_id>")
+class Store(MethodView):
+    def get(self, store_id):
+        try:
+            return stores[store_id], 200
+        except KeyError:
+            # without smorest
+            # return {"message":"store not found"}, 404
+            # with smorest
+            abort(404, message="store not found.")
+
+    def delete(self, store_id):
+        try:
+            del stores[store_id]
+            return {"message": "Store deleted."}
+        except KeyError:
+            abort(404, "Store not found")
