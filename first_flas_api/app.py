03bc04b542ac385aadaa0e85cb32f49d30ee6505
Sam <samuel.waters@gmail.com>
2023-06-26 08:54:19 -0600

    lesson: method views

--- first_flas_api/app.py
+++ first_flas_api/app.py
@@ -22,6 +22,10 @@ app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] ="/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist" 
+
+
+
 
 
 # no longer needed because we are using blueprints
