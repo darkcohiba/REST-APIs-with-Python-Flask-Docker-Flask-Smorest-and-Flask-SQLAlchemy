227968cde8aaeefc275d11cc4fe0fba1e2c30fbb
Sam <samuel.waters@gmail.com>
2023-06-28 13:02:28 -0600

    lesson: methodviews, set up post for item

--- first_flas_api/resources/item.py
+++ first_flas_api/resources/item.py
@@ -40,3 +40,35 @@ class Item(MethodView):
             return item
         except KeyError:
             abort(404, message="item not found.")
+
+@blp.route("/item")
+class ItemList(MethodView):
+    def get(self):
+        return {"items": list(items.values())}
+
+    def post(self):
+        item_data = request.get_json()
+        # Here not only we need to validate data exists,
+        # But also what type of data. Price should be a float,
+        # for example.
+        if (
+            "price" not in item_data
+            or "store_id" not in item_data
+            or "name" not in item_data
+        ):
+            abort(
+                400,
+                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+            )
+        for item in items.values():
+            if (
+                item_data["name"] == item["name"]
+                and item_data["store_id"] == item["store_id"]
+            ):
+                abort(400, message=f"Item already exists.")
+
+        item_id = uuid.uuid4().hex
+        item = {**item_data, "id": item_id}
+        items[item_id] = item
+
+        return item
