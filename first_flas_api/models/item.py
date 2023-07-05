cdc2c8dddad64a0c3c2a125d786378f6091d5f6d
Sam <samuel.waters@gmail.com>
2023-07-05 10:39:05 -0600

    lesson: simple sqlalchemy model

--- first_flas_api/models/item.py
+++ first_flas_api/models/item.py
@@ -4,3 +4,7 @@ class ItemModel(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    # the name can not be longer then 80 characters, it must be unique and it can not be null
+    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
+    store_id = db.Column(db.Integer, unique=False, nullable=False)
