4d6de1e94aaa1abfc31f43f6e4a951c4d1eb7ab7
Sam <samuel.waters@gmail.com>
2023-07-05 11:03:57 -0600

    lesson: one to many relationships

--- first_flas_api/models/store.py
+++ first_flas_api/models/store.py
@@ -7,3 +7,4 @@ class StoreModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     # the name can not be longer then 80 characters, it must be unique and it can not be null
     item = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
+    # lazy = dynamic means the items wont be fetched until we fetch it
