ef41ae1a3e0f9e22bcac3b3ccd1c415fd97b9321
Sam <samuel.waters@gmail.com>
2023-07-05 11:02:30 -0600

    lesson: one to many relationships

--- first_flas_api/models/item.py
+++ first_flas_api/models/item.py
@@ -7,4 +7,5 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     # the name can not be longer then 80 characters, it must be unique and it can not be null
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
-    store_id = db.Column(db.Integer, unique=False, nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
+    store = db.relationship("StoreModel", back_populates="items")
--- first_flas_api/models/store.py
+++ first_flas_api/models/store.py
@@ -6,3 +6,4 @@ class StoreModel(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     # the name can not be longer then 80 characters, it must be unique and it can not be null
+    item = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
