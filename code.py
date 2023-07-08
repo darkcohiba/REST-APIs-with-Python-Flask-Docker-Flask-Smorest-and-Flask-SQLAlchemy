630ba2716a99e98dd4bdc4a0c0c51559c7e1daac
Sam <samuel.waters@gmail.com>
2023-07-07 22:15:12 -0600

    chore: update

--- code.py
+++ code.py
@@ -1,15 +1,1 @@
-from typing import Float
-
-
-arr = [1, 2, 3, 4, 8, 3.2, 5, 2, 3, .5, ]
-
-print(arr[int((len(arr)/2) -0.5)])
-
-from typing import Float
-
-for num in arr:
-  if (type(num) is Float):
-    print(num)
-
-
-from datetime import datetime
+print("Hello World")
