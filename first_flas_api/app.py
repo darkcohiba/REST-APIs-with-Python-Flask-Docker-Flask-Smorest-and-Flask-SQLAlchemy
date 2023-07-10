c4ff4276c75d773e773f207f445f00a457a5332e
Sam <samuel.waters@gmail.com>
2023-07-09 19:36:44 -0600

    lesson: configure flask sqlalchemy

--- first_flas_api/app.py
+++ first_flas_api/app.py
@@ -37,7 +37,8 @@ def create_app():
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] ="/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/" 
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
 
     # create our api endpoints
