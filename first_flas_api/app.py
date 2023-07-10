cee8bbb108c79c9f6a263685d68f5d2b7eb60f94
Sam <samuel.waters@gmail.com>
2023-07-09 19:39:03 -0600

    lesson: configure flask sqlalchemy

--- first_flas_api/app.py
+++ first_flas_api/app.py
@@ -41,7 +41,7 @@ def create_app():
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     # import our sqlite through os to protect our system database and give it a default
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///data.db")
 
 
     # create our api endpoints
